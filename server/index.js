80cc2d63afa1b617aad9c7b5643ba3ee654f1904
Gavan Browne <gavan.browne@gmail.com>
2024-10-28 16:44:14 +0000

    add messages to/from client/server
    Removed node_modules from tracking. Doh. Basic example is finished!

--- server/index.js
+++ server/index.js
@@ -8,4 +8,17 @@ const wss = new WebSocket.Server({ port: 8082 });
 // a user with multiple connections or many users will lead to many different ws objects
 wss.on("connection", (ws) => {
   console.log("new client connected!");
+
+  // receive a message from client and log
+  ws.on("message", (data) => {
+    console.log(`client has sent us: ${data}`);
+    // console.log(data.toString().toUpperCase());
+    // send message to client, just send what we received as uppercase
+    ws.send(data.toString().toUpperCase());
+  });
+
+  // note how the close refers to the single connection (ws) and not the server (wss)
+  ws.on("close", () => {
+    console.log("client has disconnected!");
+  });
 });
